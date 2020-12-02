d77f7da49203540132e9ada32a2081c4ccc2d45c
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2020-12-02 13:43:48 -0600

    Maquetación y estilos del flujo de pago .

--- src/containers/Success.js
+++ src/containers/Success.js
@@ -2,7 +2,15 @@ import React from 'react';
 import '../styles/components/Success.css';
 
 const Success = () => {
-  return <div>Success</div>;
+  return (
+    <div className="Succes">
+      <div className="Success-content">
+        <h2>Nombre, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
